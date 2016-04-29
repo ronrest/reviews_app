a5ee6ef71b5c9751ffb1ec31bf3196b034edaefd
ronrest <ronny.coding@gmail.com>
2016-04-29 19:38:10 +1000

    ADMIN - Add basic admin functionality

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Review, User, Item
+
 # Register your models here.
+admin.site.register(Review)
+admin.site.register(User)
+admin.site.register(Item)

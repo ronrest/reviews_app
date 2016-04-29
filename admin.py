8654c7806a8f2412b7992b2629373a729a30d4b2
ronrest <ronny.coding@gmail.com>
2016-04-29 19:52:59 +1000

    ADMIN - Add custom admin features for User Model

--- admin.py
+++ admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 from .models import Review, User, Item
 
-# Register your models here.
+# ==============================================================================
+#                                                                     USER ADMIN
+# ==============================================================================
+class UserAdmin(admin.ModelAdmin):
+    """
+    Customise the admin page for the User Model so it displays first and
+    last names as separate columns, as well as email.
+
+    Also has filtering and search features.
+    """
+    list_display = ('first_name', 'last_name', 'email')
+    list_filter = ("last_name",)
+    search_fields = ("last_name", "first_name")
+
+
+# ==============================================================================
+#                                                                REGISTER MODELS
+# ==============================================================================
 admin.site.register(Review)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Item)

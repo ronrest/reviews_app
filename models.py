dcf479ca19492648ac2afa90b22b64d8aebb7aaa
ronrest <ronny.coding@gmail.com>
2016-04-29 17:31:49 +1000

    MODELS - Add a Reviews Model

--- models.py
+++ models.py
@@ -15,9 +15,32 @@ class Item(models.Model):
 
 
 # ==============================================================================
+#                                                                         REVIEW
+# ==============================================================================
+class Review(models.Model):
+    # Restrict ratings to be categorical
+    RATING_CHOICES = (
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+    )
+
+    item = models.ForeignKey(Item, on_delete="CASCADE")
+    author = models.ForeignKey(User, on_delete="CASCADE")
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    review = models.TextField(max_length=250)
+    pub_date = models.DateTimeField(auto_now=False,
+                                    auto_now_add=False,
+                                    verbose_name="date published")
+
+
+# ==============================================================================
 #                                                                           USER
 # ==============================================================================
 class User(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=150, blank=True, null=False)
+

6f305d7fe4a10a0f1cdae27817489620db64e312
ronrest <ronny.coding@gmail.com>
2016-04-29 17:35:09 +1000

    MODELS - Add the default string representation for the User Model

--- models.py
+++ models.py
@@ -46,3 +46,9 @@ class User(models.Model):
 
     def get_full_name(self):
         return self.first_name + " " + self.last_name
+
+    def __unicode__(self):
+        return self.get_full_name()
+
+    def __str__(self):
+        return self.get_full_name()

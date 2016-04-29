22284031b48d37cd3aede2c9cc58b36facbd11cd
ronrest <ronny.coding@gmail.com>
2016-04-29 21:51:50 +1000

    ADMIN - Add custom Admin Class for Reviews

--- admin.py
+++ admin.py
@@ -18,8 +18,27 @@ class UserAdmin(admin.ModelAdmin):
 
 
 # ==============================================================================
+#                                                                   REVIEW ADMIN
+# ==============================================================================
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Customise the admin page for the User Model so it displays first and
+    last names as separate columns, as well as email.
+
+    Also has filtering and search features.
+    """
+    list_display = ('item', 'rating', "author","condensed_review", 'time_condensed')
+    list_filter = ("pub_date", "rating")
+    search_fields = ("item__name",)
+
+    def time_condensed(self, obj):
+        return obj.pub_date.strftime("%Y_%m_%d %H:%M")
+
+
+# ==============================================================================
 #                                                                REGISTER MODELS
 # ==============================================================================
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(Item)
+

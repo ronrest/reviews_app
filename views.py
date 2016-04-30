35b1b98b390c1fda81165a6934a60fdd6b6b5fce
ronrest <ronny.coding@gmail.com>
2016-04-30 16:06:26 +1000

    VIEWS/TEMPLATES - Split template, and placed in app subdirs - Split the template into a base skeleton template and template for the specific view. - Also placed them in a subdir for the app name - Also made stored the link to the subdirs as variables to make it easier to make changes in the future

--- views.py
+++ views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 from .models import Review, Item, User
 
+template_sub_dir = "reviews/"   # Used for locating templates for this app
+static_sub_dir = "reviews/"   # Used for locating static files for this app
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("hey hey hey!!!")
@@ -12,8 +16,10 @@ def index(request):
 # ==============================================================================
 def review_list(request):
     reviews = Review.objects.all()
-    context = {"reviews":reviews}
+    context = {"reviews":reviews,
+               "page_title":"Latest Reviews",
+               }
     return render(request,
-                  template_name="review_list_template.html",
+                  template_name= template_sub_dir + "review_list_template.html",
                   context=context)
 

0a1063b27d38d1ee42c28c14c465f99a11180a5d
ronrest <ronny.coding@gmail.com>
2016-04-30 14:46:57 +1000

    VIEWS/URLS/TEMPLATES - populate the index with list of reviews

--- views.py
+++ views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Review, Item, User
+
 # Create your views here.
 def index(request):
     return HttpResponse("hey hey hey!!!")
 
+
+# ==============================================================================
+#                                                                    REVIEW_LIST
+# ==============================================================================
+def review_list(request):
+    reviews = Review.objects.all()
+    context = {"reviews":reviews}
+    return render(request,
+                  template_name="review_list_template.html",
+                  context=context)
 

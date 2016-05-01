f7bcaa83cb5111ee14281be068084d127f6c5d24
ronrest <ronny.coding@gmail.com>
2016-05-01 16:41:04 +1000

    URLS - grouped urlconf sections by adding spacing

--- urls.py
+++ urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.review_list, name="review_list"),
-    url(r'^item/$', views.item_list, name="item_list"),
     url(r'^(?P<review_id>[0-9]+)/$', views.single_review, name="single_review"),
+
+    url(r'^item/$', views.item_list, name="item_list"),
     url(r'^item/(?P<item_id>[0-9]+)/$', views.single_item, name="single_item"),
+
     url(r'^add_review/(?P<item_id>[0-9]+)/$', views.add_review, name="add_review"),
 ]
